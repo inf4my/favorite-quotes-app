147def5e1a21445e673b1d7a1a6cd26334602605
LABC706-11 <LABC706-11@EXAM.UMN.AC.ID>
2017-09-07 10:43:32 +0700

    udah ada library page

---
+++ src/pages/library/library.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
+
+/**
+ * Generated class for the LibraryPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-library',
+  templateUrl: 'library.html',
+})
+export class LibraryPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  quoteCollection: {
+    category: string,
+    quotes: Quote[],
+    icon:string
+  }[];
+
+  ngOnInit(){
+    this.quoteCollection = quotes;
+  }
+  ionViewDidLoad() {
+  }
+  
+
+  }
